7fecb2e675031925d451414f7bb7b5b6f5e1f460
yinansong <yinan@yinansong.com>
2014-11-15 11:13:32 -0500

    add the minimal seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+collector01 = Collector.create(
+  headshot_url: "",
+  name: "yinan",
+  string: "yinan.e.song@gmail.com",
+  password_digest: "manholes4eva"
+)
+
+mhcv01 = Manholecovers.create(
+  manhole_url: "http://photos-a.ak.instagram.com/hphotos-ak-xaf1/10507830_783192385081928_661478825_n.jpg",
+  country: "China",
+  region: "Sichuan",
+  city: "Chengdu",
+  year: "2014",
+  color: "black",
+  keywords: "frogs", "traditional",
+  notes: "wow so fun!",
+  featured: true
+)
+
+mhcv.collector_id = collector01.id
