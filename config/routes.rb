c95685ebe3c4e34a318122c7f4ed3cb41ef38bad
yinansong <yinan@yinansong.com>
2015-01-14 17:59:56 -0500

    add error handling

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,13 @@ Rails.application.routes.draw do
 
   get 'signup' => 'collectors#new'
 
+  # error handling
+  match '/404', via: :all, to: 'errors#not_found'
+  match '/422', via: :all, to: 'errors#unprocessable_entity'
+  match '/500', via: :all, to: 'errors#server_error'
+
+  match '*path', via: :all, to: 'errors#not_found'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
