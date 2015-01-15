740205a359b2714ebc3882413a5862e6caa989f7
yinansong <yinan@yinansong.com>
2015-01-15 17:18:36 -0500

    migrated to include 'avartar' for collectors

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115154906) do
+ActiveRecord::Schema.define(version: 20150115220851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "collectors", force: true do |t|
     t.string   "headshot_url"
-    t.string   "name",            null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
+    t.string   "name",                null: false
+    t.string   "email",               null: false
+    t.string   "password_digest",     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "manholecovers", force: true do |t|
