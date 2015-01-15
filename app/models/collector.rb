045933c6458ac20babf1df35e13544958168b187
yinansong <yinan@yinansong.com>
2015-01-15 17:08:15 -0500

    add avartar to Collector model

--- app/models/collector.rb
+++ app/models/collector.rb
@@ -1,3 +1,12 @@
 class Collector < ActiveRecord::Base
   has_many :manholecovers
+
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attach_file :avatar, styles: {
+    thumb: '50x50>',
+    circle: '200x200>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
