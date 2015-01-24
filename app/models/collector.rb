d28afe611daac6e0359a538d24de24207859e59b
yinansong <yinan@yinansong.com>
2015-01-24 15:56:05 -0500

    keep the modesl dry with s3 and paperclip

--- app/models/collector.rb
+++ app/models/collector.rb
@@ -4,10 +4,6 @@ class Collector < ActiveRecord::Base
   has_attached_file :avatar,
     :styles => {:thumb => '50x50>', :medium => '200x200>'},
     :default_url => ':style/missing.jpg',
-    # The :url is the url that users will use to render the image.
-    :url => ':s3_domain_url',
-    # The :path is the directory in your application where your files will be stored.
-    :path => '/:class/:id_partition/:style/:filename',
     :storage => :s3,
     :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => S3_CREDENTIALS
