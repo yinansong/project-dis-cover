a56b0f1ae44635ef14dc22d7181236559c63999c
yinansong <yinan@yinansong.com>
2015-01-26 22:22:13 -0500

    remove the extra has_attached_file from manholecover.rb & increase the allowded sizes to 2mb for both manholecover & collector

--- app/models/manholecover.rb
+++ app/models/manholecover.rb
@@ -7,10 +7,8 @@ class Manholecover < ActiveRecord::Base
     :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => S3_CREDENTIALS
 
-  has_attached_file :manhole_img
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :manhole_img, :content_type => /\Aimage\/.*\Z/
   validates_attachment_file_name :manhole_img, :matches => [/png\Z/, /jpe?g\Z/]
-  validates_with AttachmentSizeValidator, :attributes => :manhole_img, :less_than => 1.megabytes
-
+  validates_with AttachmentSizeValidator, :attributes => :manhole_img, :less_than => 2.megabytes
 end
