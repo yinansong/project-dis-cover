a0704e65a71774f3f7a41bac0029dd10abff6700
yinansong <yinan@yinansong.com>
2015-03-25 15:36:16 -0400

    update disqus short name

--- config/initializers/monologue.rb
+++ config/initializers/monologue.rb
@@ -10,7 +10,7 @@ Monologue.config do |config|
   config.admin_force_ssl = false
   config.posts_per_page = 10
 
-  config.disqus_shortname = "project-dis-cover-blog"
+  config.disqus_shortname = "project-discover"
   # COME-BACK NOTE: this needs to be changed!!!
 
   # LOCALE
