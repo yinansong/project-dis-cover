3ec0b948cb74768d4094a62940bb9c622f2dfab3
yinansong <yinan@yinansong.com>
2015-03-25 01:06:28 -0400

    change config for monologue

--- config/initializers/monologue.rb
+++ config/initializers/monologue.rb
@@ -10,7 +10,7 @@ Monologue.config do |config|
   config.admin_force_ssl = false
   config.posts_per_page = 10
 
-  config.disqus_shortname = "my_disqus_shortname" 
+  config.disqus_shortname = "my_disqus_shortname"
   # COME-BACK NOTE: this needs to be changed!!!
 
   # LOCALE
@@ -31,10 +31,10 @@ Monologue.config do |config|
 
   #SOCIAL
   # COME-BACK NOTE: this needs to be changed!!!
-  config.twitter_username = "myhandle"
-  config.facebook_url = "https://www.facebook.com/myhandle"
+  config.twitter_username = "___DisCover___"
+  config.facebook_url = "https://www.facebook.com/pages/Project-DisCover/1535689803346815"
   config.facebook_logo = 'logo.png'
-  config.google_plus_account_url = "https://plus.google.com/u/1/.../posts"
+  config.google_plus_account_url = "https://plus.google.com/u/0/108609637715305843874"
   config.linkedin_url = "http://www.linkedin.com/in/myhandle"
   config.github_username = "myhandle"
   config.show_rss_icon = true
